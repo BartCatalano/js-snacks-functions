9a2c5875c39e3cc9cb8a08f6f019c953a77c6951
BartCatalano <catalanobartolomeo@gmail.com>
2024-11-06 17:48:49 +0100

    feat: secondo esercizio fattto, sistemare solo l'inserimento dei parametri

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,31 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-function 
+/**
+ * 
+ * @param {array} array 
+ * @param {lettera} lettera 
+ * @returns 
+ */
+function selzioneIniziale (array,lettera){
+    const arraySelLettera = [];
+    for(let i = 0; i < array.length; i++ ){
+        const elemantoCorrente = array[i];
+        const iniziale = elemantoCorrente.charAt(0);
+        if (iniziale === lettera) {
+            arraySelLettera.push(iniziale);
+        }
+}
+return arraySelLettera;
+
+
+}
+
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(selzioneIniziale(names, A));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
