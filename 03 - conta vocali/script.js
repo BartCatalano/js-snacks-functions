3f2a5688587834a968f1eb143a5890ee210a68c2
BartCatalano <catalanobartolomeo@gmail.com>
2024-11-06 18:37:18 +0100

    feat: ultimo esercizio fnito, bonus (wip)

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,33 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+/**
+ * Description
+ * @param {string} stringa
+ * @returns {number}
+ */
+function vocali(stringa){
+    const vocali= `aeiou`;
+    let conteggio = 0;
+    let vocalipresenti = [];    
+    for (let i = 0; i < stringa.length; i++) {
+        const parolaCorrente = stringa[i];
+        if (vocali.includes(parolaCorrente)  ) {
+            conteggio = ++conteggio;
+            vocalipresenti.push(parolaCorrente);
 
+         } 
+    
+    }
+    return conteggio, vocalipresenti;
+}
+ 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(vocali(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+
+
